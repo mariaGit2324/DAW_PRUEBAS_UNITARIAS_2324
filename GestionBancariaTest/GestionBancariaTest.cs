c880ee90af9e91f55114b3d6d822fea2542686b4
MARIA MIRA ALAVES <marmirala@alu.edu.gva.es>
2024-01-29 00:33:21 +0100

    Realizada Actividad 7 - Crear clases de equivalencia y realizar casos de prueba + Corrección de código MMA2324

---
+++ GestionBancariaTest/GestionBancariaTest.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using GestionBancariaAppNS;
+
+namespace GestionBancariaTest
+{
+    [TestClass]
+    public class GestionBancariaTest
+    {
+        [TestMethod]
+        public void ValidarReintegro()
+        {
+            double saldoInicial = 1000;
+            double reintegro = 250;
+            double saldoEsperado = 750;
+
+            GestionBancariaApp miApp = new GestionBancariaApp(saldoInicial);
+
+            miApp.RealizarReintegro(reintegro);
+
+            Assert.AreEqual(saldoEsperado, miApp.ObtenerSaldo(), 0.001, "Se produjo un error al realizar el reintegro, saldo" + "incorrecto.");
+
+        }
+
+        [TestMethod]
+        public void ValidarIngreso()
+        {
+            double saldoInicial = 1000;
+            double cantidad = 300;
+            double saldoEsperado = 1300;
+
+            GestionBancariaApp miApp = new GestionBancariaApp(saldoInicial);
+
+            miApp.RealizarIngreso(cantidad);
+
+            Assert.AreEqual(saldoEsperado, miApp.ObtenerSaldo(), 0.001, "Se produjo un error al realizar el ingreso, saldo" + "incorrecto.");
+
+        }
+
+        [TestMethod]
+        public void ValidarReintegroLimite()
+        {
+            double saldoInicial = 500;
+            double reintegro = 0;
+            double saldoEsperado = 500;
+
+            GestionBancariaApp miApp = new GestionBancariaApp(saldoInicial);
+
+            miApp.RealizarReintegro(reintegro);
+
+            Assert.AreEqual(saldoEsperado, miApp.ObtenerSaldo(), 0.001, "Se produjo un error al realizar el reintegro, saldo" + "incorrecto.");
+
+        }
+
+        [TestMethod]
+        public void ValidarReintegroNegativo()
+        {
+            double saldoInicial = 500;
+            double reintegro = -50;
+            double saldoEsperado = 500;
+
+            GestionBancariaApp miApp = new GestionBancariaApp(saldoInicial);
+
+            miApp.RealizarReintegro(reintegro);
+
+            Assert.AreEqual(saldoEsperado, miApp.ObtenerSaldo(), 0.001, "Se produjo un error al realizar el reintegro, saldo" + "incorrecto.");
+
+        }
+
+        [TestMethod]
+        public void ValidarReintegroMayorSaldo()
+        {
+            double saldoInicial = 500;
+            double reintegro = 750;
+            double saldoEsperado = 500;
+
+            GestionBancariaApp miApp = new GestionBancariaApp(saldoInicial);
+
+            miApp.RealizarReintegro(reintegro);
+
+            Assert.AreEqual(saldoEsperado, miApp.ObtenerSaldo(), 0.001, "Se produjo un error al realizar el reintegro, saldo" + "incorrecto.");
+
+        }
+
+        [TestMethod]
+        public void ValidarIngresoNegativo()
+        {
+            double saldoInicial = 1200;
+            double cantidad = -500;
+            double saldoEsperado = 1200;
+
+            GestionBancariaApp miApp = new GestionBancariaApp(saldoInicial);
+
+            miApp.RealizarIngreso(cantidad);
+
+            Assert.AreEqual(saldoEsperado, miApp.ObtenerSaldo(), 0.001, "Se produjo un error al realizar el ingreso, saldo" + "incorrecto.");
+
+        }
+
+        [TestMethod]
+        public void ValidarIngresoLimite()
+        {
+            double saldoInicial = 1200;
+            double cantidad = 0;
+            double saldoEsperado = 1200;
+
+            GestionBancariaApp miApp = new GestionBancariaApp(saldoInicial);
+
+            miApp.RealizarIngreso(cantidad);
+
+            Assert.AreEqual(saldoEsperado, miApp.ObtenerSaldo(), 0.001, "Se produjo un error al realizar el ingreso, saldo" + "incorrecto.");
+
+        }
+    }
+}
